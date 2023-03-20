1cf637a21a2e660c365f6d43bcf9e4df0f17046e
devdunlap <108426486+devdunlap@users.noreply.github.com>
2023-03-20 14:20:51 -0500

    added color and functionality

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// Place holder for the numbers
+let num1 = "";
+let num2 = "";
+// Sets up the numbers and buttons
+const container = document.getElementById("container");
+// Gets the numbers
+const numbers = document.querySelectorAll(".number");
+// Loops through the numbers and adds event listeners
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        if (num1 === "") {
+            num1 = Number(event.target.innerText);
+        } else {
+            num2 = Number(event.target.innerText);
+        }
+        // Changes the innerHTML of the container to the numbers
+     container.innerHTML = num1 + " " + num2;  
+    });
+});
+
+
 // Get the input fields and buttons
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
+
 const addBtn = document.getElementById("add");
 const subtractBtn = document.getElementById("subtract");
 const multiplyBtn = document.getElementById("multiply");
@@ -9,17 +29,17 @@ const result = document.getElementById("result");
 
 // Add event listeners to the buttons
 addBtn.addEventListener("click", function() {
-  result.innerHTML = Number(num1.value) + Number(num2.value);
+  container.innerHTML = Number(num1) + Number(num2);
 });
 
 subtractBtn.addEventListener("click", function() {
-  result.innerHTML = Number(num1.value) - Number(num2.value);
+  container.innerHTML = Number(num1) - Number(num2);
 });
 
 multiplyBtn.addEventListener("click", function() {
-  result.innerHTML = Number(num1.value) * Number(num2.value);
+  container.innerHTML = Number(num1) * Number(num2);
 });
 
 divideBtn.addEventListener("click", function() {
-  result.innerHTML = Number(num1.value) / Number(num2.value);
+  container.innerHTML = Number(num1) / Number(num2);
 });
